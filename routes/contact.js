fbc7e0dfd6b42e705c1edf9b47b1b7906fcaa355
clark61 <Clark6147@gmail.com>
2022-04-14 18:50:13 -0500

    Update comment

--- routes/contact.js
+++ routes/contact.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* Get about page */
+/* Get contact page */
 router.get('/', function(req, res, next) {
     res.render('contact', {title: 'Contact Windy City Print Shop'});
 });
