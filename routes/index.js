aa79c01039f8d003e8dedcaf69044ed7ecb99e6d
clark61 <Clark6147@gmail.com>
2022-05-09 20:55:18 -0500

    Add products and promotions

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+let query = "SELECT id, product_name, product_type, dimensions, price, quantity FROM product";
+  // execute query
+  db.query(query, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.render('error');
+  }
+
+  let query = "SELECT id, title, image, description FROM promotion WHERE start_date <= CURRENT_DATE() and end_date >= CURRENT_DATE()";
+  // execute query
+  db.query(query, (err, result2) => {
+    if (err) {
+      console.log(err);
+      res.render('error');
+    }
+    res.render('index', {allrecs: result, promos: result2 });
+    });
+  });
 });
 
 module.exports = router;
