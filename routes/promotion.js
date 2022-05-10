5f0a83557fd01695bcca7168a65d4535f40285b4
clark61 <Clark6147@gmail.com>
2022-05-09 20:42:49 -0500

    Update promotion columns

--- routes/promotion.js
+++ routes/promotion.js
@@ -5,7 +5,7 @@ var router = express.Router();
 // Route to list all records. Display view to list all records
 // ==================================================
 router.get('/', function(req, res, next) {
-    let query = "SELECT id, percent_off, description FROM promotion";
+    let query = "SELECT id, percent_off, description, title, start_date, end_date FROM promotion";
     // execute query
     db.query(query, (err, result) => {
         if (err) {
@@ -20,7 +20,7 @@ router.get('/', function(req, res, next) {
 // Route to view one specific record. Notice the view is one record
 // ==================================================
 router.get('/:recordid/show', function(req, res, next) {
-    let query = "SELECT id, percent_off, description FROM promotion WHERE id = " + req.params.recordid;
+    let query = "SELECT id, percent_off, description, title, start_date, end_date FROM promotion WHERE id = " + req.params.recordid;
     // execute query
     db.query(query, (err, result) => {
         if (err) {
@@ -43,8 +43,8 @@ router.get('/addrecord', function(req, res, next) {
 // Route to obtain user input and save in database.
 // ==================================================
 router.post('/', function(req, res, next) {
-    let insertquery = "INSERT INTO promotion (percent_off, description) VALUES (?, ?)";
-    db.query(insertquery,[req.body.percent_off, req.body.description], (err, result) => {
+    let insertquery = "INSERT INTO promotion (percent_off, description, title, image, start_date, end_date) VALUES (?, ?, ?, ?, ?, ?)";
+    db.query(insertquery,[req.body.percent_off, req.body.description, req.body.title, req.body.image, req.body.start_date, req.body.end_date], (err, result) => {
         if (err) {
             console.log(err);
             res.render('error');
@@ -58,7 +58,7 @@ router.post('/', function(req, res, next) {
 // Route to edit one specific record.
 // ==================================================
 router.get('/:recordid/edit', function(req, res, next) {
-    let query = "SELECT id, percent_off, description FROM promotion WHERE id = " + req.params.recordid;
+    let query = "SELECT id, percent_off, description, title, image, start_date, end_date FROM promotion WHERE id = " + req.params.recordid;
     // execute query
     db.query(query, (err, result) => {
         if (err) {
@@ -74,8 +74,8 @@ router.get('/:recordid/edit', function(req, res, next) {
 // Route to save edited data in database.
 // ==================================================
 router.post('/save', function(req, res, next) {
-    let updatequery = "UPDATE promotion SET percent_off = ?, description = ? WHERE id = " + req.body.id;
-    db.query(updatequery,[req.body.percent_off, req.body.description], (err, result) => {
+    let updatequery = "UPDATE promotion SET percent_off = ?, description = ?, title = ?, image = ?, start_date = ?, end_date = ? WHERE id = " + req.body.id;
+    db.query(updatequery,[req.body.percent_off, req.body.description, req.body.title, req.body.image, req.body.start_date, req.body.end_date], (err, result) => {
         if (err) {
             console.log(err);
             res.render('error');
